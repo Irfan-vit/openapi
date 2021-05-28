85340bf2992bbbb1f602358c49d7b3d5b5dbb64d
Irfan-vit <65129024+Irfan-vit@users.noreply.github.com>
2021-05-28 00:03:48 -0700

    wip

---
+++ .history/app_20210527224625.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector('#output')
+var serverURL = "https://aws.random.cat/meow"
+var key = "&appid=49257f6591cfc3ed8daf0b5970d519cb"
+
+function getTranslationURL(text) {
+    return serverURL + text + key
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something went wrong  :  " + error)
+}
+
+function clickHandler() {
+    // outputDiv.innerText = "translated  " + txtInput.value + "  To : bababbababababababa ";
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        // .then(json => {
+        //     var translatedText = "Country : " + json['sys']['country'] + " | tempreature : " + json['main']['temp'] + " | weather : " +  json['weather'][0]['description'] + " | longitude : " + json.coord.lon + " | latitude : " + json['coord']['lat'] + " | wind speed : " + json['wind']['speed'] + " | Wind direction, degrees : " + json['wind']['deg'];
+        //     outputDiv.innerText = translatedText;
+        //     console.log()})
+
+        .then(data => {output.innerHTML = `<img src=${data} alt="cat" />`})
+    };
+
+
+btnTranslate.addEventListener("click", clickHandler)
